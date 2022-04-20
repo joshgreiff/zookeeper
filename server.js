cdadccac2f831a45f989c2f1e29b59bed47dca28
joshgreiff <josh@greiff.org>
2022-04-20 11:48:17 -0400

    finished post request and error handling for data validation

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const { animals } = require('./data/animals')
 const PORT = process.env.PORT || 3001
@@ -55,6 +57,34 @@ function findById(id, animalsArray) {
     return result
 }
 
+function createNewAnimal(body, animalsArray){
+    const animal = body
+    animalsArray.push(animal)
+    
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    )
+
+    return animal
+}
+
+function validateAnimal(animal){
+    if(!animal.name || typeof animal.name !== 'string'){
+        return false
+    }
+    if(!animal.species || typeof animal.species !== 'string'){
+        return false
+    }
+    if(!animal.diet || typeof animal.diet !== 'string'){
+        return false
+    }
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false
+    }
+    return true
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals
     if(req.query){
@@ -73,10 +103,21 @@ app.get('/api/animals/:id', (req, res) => {
 })
 
 app.post('/api/animals', (req, res) => {
+// set id based on what the next index of the array will be 
+req.body.id = animals.length.toString()
 
-// req. body is where our incoming content will be
-console.log(req.body)
-res.json(req.body)
+// if any data in req.body is incorrect, send a 400 error back
+if(!validateAnimal(req.body)){
+    res.status(400).send('The animal is not properly formatted')
+}else{
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals)
+
+    res.json(animal)
+}
+
+
+
 
 })
 
